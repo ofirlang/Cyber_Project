64f4757fac43f737a780ed648d69d4fa44b7336b
ofirl <ofirlang12@gmail.com>
2022-10-28 10:20:25 +0300

    First commit. added 4 new files

---
+++ README.md
@@ -0,0 +1,2 @@
+this repository is used to store my homework for sugar debi
+as well as the files for the final project
---
+++ ex.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def print_words(file_name):
+    word_counting = {}
+    with open(file_name, 'r', encoding='utf8') as file:
+        f = file.read()
+        f = f.split()
+        for word in f:
+            if word.strip() not in word_counting:
+                word_counting[word.strip()] = f.count(word)
+    print(word_counting)
+    return word_counting
+
+
+def print_top(file_name):
+    word_counting = print_words(file_name)
+    sorted_counting = {k: v for k, v in sorted(word_counting.items(), key=lambda item: item[1])}
+    for i in list(sorted_counting.items())[-20:]:
+        print(i)
+
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('usage: ./wordcount.py {--count | --topcount} file')
+        sys.exit(1)
+
+    option = sys.argv[1]
+    file_name = sys.argv[2]
+    if option == '--count':
+        print_words(file_name)
+    elif option == '--topcount':
+        print_top(file_name)
+    else:
+        print('unknown option: ' + option)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
