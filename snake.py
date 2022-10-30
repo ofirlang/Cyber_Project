e5eea3bb6f5177d0529f069bf1133e6e8b964798
ofirl <ofirlang12@gmail.com>
2022-10-30 20:11:19 +0200

    two files modified: snake.py, test.py

--- snake.py
+++ snake.py
@@ -2,7 +2,6 @@ import sys
 
 
 def main():
-    print("hi")
     encryption = {'s': 'L', 'b': 's', 'w': 'O', 'z': 'G', 'c': 'o', 'J': 'y', 'V': 't', 'P': 'w', 'B': 'f', 'Z': 'q', 'F': 'k', 'O': 'N', 'u': 'A', 'W': 'r', 'K': 'K', 'a': 'D', 'v': 'l',
     'g': 'S', 'f': 'x', 'x': 'c', 'N': 'e', 'p': 'b', 'U': 'a', 'j': 'P', 'o': 'Q', 'i': 'I', 'M': 'd', 't': 'U', 'H': 'V', 'X': 'i', 'Y': 'T', 'R': 'H', 'h': 'X', 'L': 'z',
     'G': 'F', 'A': 'W', 'm': 'n', 'T': 'u', 'l': 'B', 'C': 'Z', 'q': 'p', 'D': 'v', 'I': 'g', 'n': 'h', 'y': 'C', 'S': 'j', 'k': 'M', 'd': 'J', 'Q': 'E', 'e': 'Y', 'r': 'R',
@@ -10,14 +9,17 @@ def main():
     encryption = {v: k for k, v in encryption.items()}
     file_name = sys.argv[1]
     with open(file_name, 'r') as encrypted:
-        snake_txt = encrypted.read()
-        for i in snake_txt:
+        snake = encrypted.read()
+        snake_txt = ''
+        for i in snake:
+            snake_txt += i
             if i.isdigit():
-                i = " "
+                snake_txt += " "
         snake_txt = snake_txt.split()
+        word1 = ''
         for word in snake_txt:
             for letter in word:
-                pass
+                word1 += encryption[letter]
 
 
 if __name__ == "__main__":
