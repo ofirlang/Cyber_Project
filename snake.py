f77bc5590b7733b2ac993579e32628b557296eeb
ofirl <ofirlang12@gmail.com>
2022-10-31 08:35:25 +0200

    added changes to snake.py and checked something in snake.txt

--- snake.py
+++ snake.py
@@ -6,20 +6,42 @@ def main():
     'g': 'S', 'f': 'x', 'x': 'c', 'N': 'e', 'p': 'b', 'U': 'a', 'j': 'P', 'o': 'Q', 'i': 'I', 'M': 'd', 't': 'U', 'H': 'V', 'X': 'i', 'Y': 'T', 'R': 'H', 'h': 'X', 'L': 'z',
     'G': 'F', 'A': 'W', 'm': 'n', 'T': 'u', 'l': 'B', 'C': 'Z', 'q': 'p', 'D': 'v', 'I': 'g', 'n': 'h', 'y': 'C', 'S': 'j', 'k': 'M', 'd': 'J', 'Q': 'E', 'e': 'Y', 'r': 'R',
     'E': 'm'}
+
     encryption = {v: k for k, v in encryption.items()}
     file_name = sys.argv[1]
+
     with open(file_name, 'r') as encrypted:
         snake = encrypted.read()
         snake_txt = ''
+
         for i in snake:
-            snake_txt += i
             if i.isdigit():
                 snake_txt += " "
+            else:
+                snake_txt += i
+
         snake_txt = snake_txt.split()
-        word1 = ''
+        decoded = ''
+
         for word in snake_txt:
             for letter in word:
-                word1 += encryption[letter]
+                if letter.isalpha():
+                    decoded += encryption[letter]
+            decoded += " "
+
+        decoded = decoded.split()
+
+        for word in decoded:
+            word = word[::-1]
+
+        decoded = " ".join(decoded)
+
+        if len(sys.argv) < 2:
+            print("file is missing")
+        elif len(sys.argv) > 2:
+            print("remove unused parameter")
+        else:
+            print(decoded)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-l = ["d", "f", "v", "b"]
-l[1] = "e"
-print(l)
+decoded = "ehT neZ fo nohtyP yb miT lufituaeB sreteP si retteb naht ticilpxE ylgu si retteb naht elpmiS ticilpmi si retteb naht xelpmoC xelpmoc si retteb naht talF detacilpmoc si retteb naht esrapS detsen si retteb naht ytilibadaeR esned laicepS stnuoc sesac tnera laiceps hguone ot kaerb eht hguohtlA selur ytilacitcarp staeb srorrE ytirup dluohs reven ssap sselnU yltnelis ylticilpxe nI decnelis eht ecaf fo ytiugibma esufer eht noitatpmet ot erehT sseug dluohs eb eno dna ylbareferp ylno eno suoivbo yaw ot od hguohtlA ti taht yaw yam ton eb suoivbo ta tsrif sselnu eruoy woN hctuD si retteb naht hguohtlA reven reven si netfo retteb naht thgir fI won eht noitatnemelpmi si drah ot nialpxe sti a dab fI aedi eht noitatnemelpmi si ysae ot nialpxe ti yam eb a doog secapsemaN aedi era eno gniknoh taerg aedi stel od erom fo esoht"""
+decoded = decoded.split()
+for word in decoded:
+    word = word[::-1]
+    print(word + " ", end='')
+
+print()
+print(decoded)
